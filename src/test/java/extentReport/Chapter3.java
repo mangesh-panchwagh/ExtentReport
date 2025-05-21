8e429fa21d3cf3260f46abf50874a188b55277d6
Mangesh Panchwagh <mangesh013@gmail.com>
2025-05-21 10:09:10 +0530

    Chapter 3

--- src/test/java/extentReport/Chapter3.java
+++ src/test/java/extentReport/Chapter3.java
@@ -1,5 +1,12 @@
 package extentReport;
 
+import com.aventstack.extentreports.ExtentReports;
+
 public class Chapter3 {
 
+	public static void main(String[] args) {
+		
+		ExtentReports extentReports = new ExtentReports();
+		
+	}
 }
